16dba0c910c91224dbc9c18f140957e972882eca
Aldo Pramilhiawan <aldopramilhiawan0@gmail.com>
2022-03-28 21:22:43 +0700

    baru

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const productRouter = require('./app/product/routes');
+const logger = require('morgan');
+
+app.use(logger('dev'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use('/public', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/v1', productRouter);
+app.use((req, res, nexrt) => {
+    res.status(404)
+    res.send({
+        status: 'failed',
+        message: "Resource" + req.originalUrl + "Not Found"
+    })
+})
+app.listen(3000, () => console.log('server: http://localhost:3000'))
